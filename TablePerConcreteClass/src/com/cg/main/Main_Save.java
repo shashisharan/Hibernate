bf2ac87afb1688078c13fbe999b40a1d6f4c77f2
shashisharan <shashisharan619club@gmail.com>
2017-09-02 18:49:04 +0530

    table  per concrete class- new table will  be created for subclasses only

---
+++ TablePerConcreteClass/src/com/cg/main/Main_Save.java
@@ -0,0 +1,38 @@
+package com.cg.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.cg.pojo.OfflineOrder;
+import com.cg.pojo.OnlineOrder;
+
+
+public class Main_Save {
+	
+
+	public static void main(String[] args) {
+		
+		Configuration cf=new Configuration();
+		cf.configure("hibernate.cfg.xml");
+		//cf.addXML("hibernate.cfg.xml");
+		
+		SessionFactory factory=cf.buildSessionFactory();
+		Session session=factory.openSession();
+		Transaction tx=session.beginTransaction();
+		
+		OfflineOrder o= new OfflineOrder();
+		o.setOrderId(1);
+		o.setOrderAmount(50000);
+		o.setOffLineOrderType("HDFC");
+		
+		session.save(o);
+		
+		tx.commit();
+		session.close();
+	
+		
+	}
+
+}
---
+++ TablePerConcreteClass/src/com/cg/pojo/OfflineOrder.java
@@ -0,0 +1,17 @@
+package com.cg.pojo;
+
+public class OfflineOrder extends Order{
+	
+	private String offLineOrderType;
+
+	public String getOffLineOrderType() {
+		return offLineOrderType;
+	}
+
+	public void setOffLineOrderType(String offLineOrderType) {
+		this.offLineOrderType = offLineOrderType;
+	}
+	
+		
+
+}
