8560d10e1c31518273f59f6c711681e8cd8becc6
shashisharan <shashisharan619club@gmail.com>
2017-09-02 18:39:55 +0530

    Table per sub class hirearchy...new table created for subclass and there is table for supr class also.

---
+++ TablePersubClassHierarchy/src/com/cg/pojo/OfflineOrder.java
@@ -0,0 +1,24 @@
+package com.cg.pojo;
+
+public class OfflineOrder extends Order{
+	
+	
+	
+	
+	public OfflineOrder() {
+		super();
+	}
+
+	private String offLineOrderType;
+
+	public String getOffLineOrderType() {
+		return offLineOrderType;
+	}
+
+	public void setOffLineOrderType(String offLineOrderType) {
+		this.offLineOrderType = offLineOrderType;
+	}
+	
+		
+
+}
---
+++ TablePersubClassHierarchy/src/com/cg/pojo/OnlineOrder.java
@@ -0,0 +1,25 @@
+package com.cg.pojo;
+
+public class OnlineOrder{
+	
+	
+
+	public OnlineOrder() {
+		super();
+	}
+
+	private String onlineOrderType;
+
+	public String getOnlineOrderType() {
+		return onlineOrderType;
+	}
+
+	public void setOnlineOrderType(String onlineOrderType) {
+		this.onlineOrderType = onlineOrderType;
+	}
+	
+	
+	
+	
+	
+}
